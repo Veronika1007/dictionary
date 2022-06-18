033e5143f39632412840964fd666bb9947c43918
Veronika1007 <veronika.horatschek@gmail.com>
2022-06-18 20:52:59 +0100

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Learn React</header>
+      <header>Dictionary</header>
+      <button className="btn btn-primary">Hi</button>
     </div>
   );
 }
